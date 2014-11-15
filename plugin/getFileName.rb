a52a26edfcc9f58c3e6797fa4283adf9f7773bf0
ysxun <ysxun@ubuntu-xyong.(none)>
2014-11-15 18:57:59 +0800

    plugin

--- plugin/getFileName.rb
+++ plugin/getFileName.rb
@@ -5,14 +5,16 @@ p = Pathname.new(File.dirname(__FILE__)).realpath
 
 #Find.find('/home/jdi/ysx/plugin') do |path|
 path = ARGV[0]
-puts p
+
 if path.index(".graph")
 	dotPath = path.gsub(".graph",".dot")
 	tempSvgPath = path.gsub(".graph","_temp.svg")
-	svgPath = path.gsub(".graph","_done.svg")
+	svgPath = path.gsub(".graph",".svg")
 		
 	system "ruby #{p}/dot.rb #{path} #{dotPath}"
 	system "dot -Tsvg #{dotPath} -o #{tempSvgPath}"
 	system "ruby #{p}/th_pic.rb #{tempSvgPath} #{svgPath} aaa bbb ccc"
+	
+	#system "cp #{svgPath} #{ARGV[1]}"
 end
 
